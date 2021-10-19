958361e00d1fd216dcfe99e4a07893894f3229b1
LivSik <79753231+RenKus96@users.noreply.github.com>
2021-10-19 17:56:34 +0300

    upd

--- src/database.py
+++ src/database.py
@@ -2,9 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import os
+
+
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://user_pg:user_pg@localhost:5432/appdb')
+
 #SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://user_pg:user_pg@localhost:5432/db"
-#SQLALCHEMY_DATABASE_URL = "postgresql://user_pg:user_pg@db:5432/db"
+#SQLALCHEMY_DATABASE_URL = "postgresql://user_pg:user_pg@localhost:5432/appdb"
+#SQLALCHEMY_DATABASE_URL = "postgresql://user_pg:user_pg@db:5432/appdb"
 
 engine = create_engine(
     # SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
